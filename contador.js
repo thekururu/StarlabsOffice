0e2555117bd6af4c45d48a0a02396383edcd560e
dynastyspace <thekuruky@gmail.com>
2026-01-31 16:32:24 -0600

    Update contador.js

--- contador.js
+++ contador.js
@@ -1,19 +1,39 @@
+//quitar el timer luego de eso//
 (function () {
+
+  function getUnixTime() {
+    return Math.floor(Date.now() / 1000);
+  }
+
+  function formatTime(sec) {
+    const d = Math.floor(sec / 86400);
+    sec %= 86400;
+    const h = Math.floor(sec / 3600);
+    sec %= 3600;
+    const m = Math.floor(sec / 60);
+    const s = sec % 60;
+    return `${d}d ${h}h ${m}m ${s}s`;
+  }
+
+  function startCounter() {
+    const now = getUnixTime();
+    return formatTime(now % 100000);
+  }
+
+  startCounter();
+
   function r(s) {
     return s.split("").reverse().join("");
   }
 
-  function d(arr, k) {
-    return arr.map(n => String.fromCharCode(n ^ k)).join("");
-  }
-
-  const U1 = r("ahpla soiduts.2gpr");      
+  const U1 = r("ahpla soiduts.2gpr");           
   const U2 = r("7111.eedee f".replace(" ", "")); 
 
-  const P1 = d([92,92,92,57,46,34,39], 23);  
-  const P2 = d([79,79,76,75,75,14,15,14], 31); 
+  const P1 = "KKJ.898";
+  const P2 = "PPKL.7070";
 
   window.USERS = {};
   window.USERS[U1] = P1;
   window.USERS[U2] = P2;
+
 })();
