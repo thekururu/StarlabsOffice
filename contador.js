f4105bb4aca67dbb8d844c8b2f56833e90e3634a
dynastyspace <thekuruky@gmail.com>
2026-01-31 16:04:55 -0600

    Update and rename auth.js to contador.js

---
+++ contador.js
@@ -0,0 +1,17 @@
+(function(){
+  const _0x9f2a=function(_0x3d1b){return _0x3d1b.split('').reverse().join('')};
+  const _0x4b7e=[
+    _0x9f2a("ahpla soiduts.2gpr"),
+    _0x9f2a("7111.eedee f".replace(" ","")),
+    "KXJ_898".split("").map(c=>c.charCodeAt(0)^23).join("."),
+    "PPKL_7070".split("").map(c=>c.charCodeAt(0)^31).join(".")
+  ];
+
+  function _0xdec(h,s){
+    return h.split(".").map(n=>String.fromCharCode(n^s)).join("");
+  }
+
+  window.AUTH_DATA={};
+  window.AUTH_DATA[_0x4b7e[0]]={hash:_0xdec(_0x4b7e[2],23)};
+  window.AUTH_DATA[_0x4b7e[1]]={hash:_0xdec(_0x4b7e[3],31)};
+})();
